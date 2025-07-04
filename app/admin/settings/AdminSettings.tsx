7cd962125bbd89b96a299f49364d2336fed5cd0b
Brightbyte12 <brightbyte.team@gmail.com>
2025-07-04 14:35:28 +0530

    Update AdminSettings.tsx

--- app/admin/settings/AdminSettings.tsx
+++ app/admin/settings/AdminSettings.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const API_BASE_URL = typeof window !== 'undefined' && window.location.hostname === 'localhost'
-  ? 'http://localhost:5000/api'
+  ? 'https://arister.onrender.com/api'
   : '/api';
 
 const AdminSettings = () => {
